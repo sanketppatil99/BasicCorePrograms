3556849164ac173b29159d14f5dc760af0ff01ec
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-01 12:00:41 +0530

    Program for FlipCoin

---
+++ BasicCorePrograms/BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        public static void flipCoin()
+        {
+            int HeadCount = 0;
+            int TailCount = 0;
+            Console.WriteLine("Enter the number of times to Flip Coin =  ");
+            int flipCoin = Convert.ToInt32(Console.ReadLine());
+
+            if (flipCoin > 0)
+            {
+                for (int i = 0; i < flipCoin; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+
+                    if (number == 0)
+                    {
+                        HeadCount++;
+                    }
+                    else
+                    {
+                        TailCount++;
+                    }
+                }
+                Console.WriteLine("Toatal HeadCount is = " + HeadCount);
+                Console.WriteLine("Toatl TailCount is = " + TailCount);
+
+                double HeadPercentage = HeadCount * 100 / flipCoin;
+                double TailPercentage = TailCount * 100 / flipCoin;
+
+                Console.WriteLine("HeadPercentage is = {0}%", HeadPercentage);
+                Console.WriteLine("TailPercentage is = {0}%", TailPercentage);
+
+            }
+            else
+            {
+                Console.WriteLine("Enter the greater than Zero");
+            }
+        }
+    }
+}
---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using BasicCorePrograms;
+using System;
+
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BasicCoreProgram");
+
+            FlipCoin.flipCoin();
+        }
+    }
+}
