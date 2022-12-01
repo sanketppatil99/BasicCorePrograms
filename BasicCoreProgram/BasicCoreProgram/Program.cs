41d71560eadc6d838b24ab95a979c932e7061ff3
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-01 15:10:48 +0530

    Program to find Quotient And Remainder

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -8,7 +8,7 @@ namespace BasicCoreProgram
         public static void Main(string[] args)
         {
 
-            PrimeFactorision.primeFactor();
+            QuotientRemainder.checkQuotientRemainder();
 
         }
     }
---
+++ BasicCoreProgram/BasicCoreProgram/QuotientRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class QuotientRemainder
+    {
+        public static void checkQuotientRemainder()
+        {
+            int dividend = 100, divisor = 10;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend: {0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Qotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
